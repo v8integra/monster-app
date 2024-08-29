9019ad92c4530dadf363ef386468e703e3068050
unknown <keith.francis83@gmail.com>
2024-08-28 21:13:08 -0600

    moved monsters to card list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -17,9 +19,6 @@ class App extends Component {
         this.setState(
           () => { 
             return { monsters: users }
-          },
-          () => {
-            console.log(this.state);
           }
         )
       );
@@ -49,16 +48,7 @@ class App extends Component {
           placeholder='search monsters' 
           onChange={ onSearchChange }
         />
-
-        {
-          filteredMonsters.map((monster) => {
-            return( 
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-          })
-        }
+        <CardList monsters={ filteredMonsters }/>
       </div>
     );
   }
