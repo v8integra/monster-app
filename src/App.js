279f46d60cffca75530ee7805599185aaf705abc
unknown <keith.francis83@gmail.com>
2024-08-29 21:58:31 -0600

    Moved search bar to seperate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/searchBox.component';
 
 import './App.css';
 
@@ -42,13 +43,13 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={ onSearchChange }
+        < SearchBox 
+          className='search-box'
+          placeholder='search monsters'
+          onChangeHandler={ onSearchChange }
         />
-        <CardList monsters={ filteredMonsters }/>
+
+        < CardList monsters={ filteredMonsters }/>
       </div>
     );
   }
