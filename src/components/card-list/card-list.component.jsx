87c6df2ec908cfa27b6fae42a6b93bb9a1b33572
unknown <keith.francis83@gmail.com>
2024-08-31 11:45:38 -0600

    Added card styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,22 @@
 import { Component } from 'react';
+import './card-list.styles.css';
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
+
     return (
-      <div>
-        { monsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        ))}
+      <div className='card-list'>
+        { monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className='card-container' key={id}>
+              <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+              <h2>{ name }</h2>
+              <p>{ email }</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
