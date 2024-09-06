da04ece3f104074f7911844e94a502c269394260
unknown <keith.francis83@gmail.com>
2024-09-05 21:51:14 -0600

    Added useEffects for fetch and filter monsters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { Component } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/searchBox.component';
@@ -9,6 +9,22 @@ import './App.css';
 const App = () => {
 
   const [searchString, setSearchString] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => setMonsters(users))
+  }, [])
+
+  useEffect(() => {
+    const updatedFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchString);
+    });
+
+    setFilteredMonsters(updatedFilteredMonsters);
+  }, [monsters, searchString])
 
   const onSearchChange = (event) => {
     const searchField = event.target.value.toLocaleLowerCase();
@@ -19,13 +35,13 @@ const App = () => {
     <div className="App">
       <h1 className='title'>Monsters List</h1>
 
-      { < SearchBox 
+      < SearchBox 
         className='search-box'
         placeholder='search monsters'
         onChangeHandler={ onSearchChange }
       />
 
-      /* < CardList monsters={ filteredMonsters }/> */}
+      < CardList monsters={ filteredMonsters }/>
     </div>
   )
 }
